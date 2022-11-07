997ea44775887befb4fd9433f6c6bb2f8dad7336
ThibaudPiccinali <100284370+ThibaudPiccinali@users.noreply.github.com>
2022-11-07 23:28:46 +0100

    Rajout de fonctions permettant le déplacement du drône

--- Deplacement du drone/main.py
+++ Deplacement du drone/main.py
@@ -5,19 +5,23 @@ from djitellopy import Tello
 
 myDrone = Tello()   # create drone object
 myDrone.connect()   # connect drone
+myDrone.enable_mission_pads()
 print(myDrone.get_battery())    # display drone battery
-myDrone.streamon()
-frame_read = myDrone.get_frame_read()
+#myDrone.streamon()
+#frame_read = myDrone.get_frame_read()
 
 
 # Takeoff
-myDrone.takeoff()
+#myDrone.takeoff()
 # Avance tout droit
-myDrone.move_forward(100)
+#myDrone.move_forward(50) ## déplacement en cm
 #Tourne
 #myDrone.rotate_clockwise(90)
+#myDrone.flip('r')
+#cv2.imwrite("picture.png", frame_read.frame)
 time.sleep(3)
-myDrone.flip('r')
-cv2.imwrite("picture.png", frame_read.frame)
+mission_pad_number=myDrone.get_mission_pad_id()
+myDrone.disable_mission_pads()
+print("identifiant : "+str(mission_pad_number))
 # Land
-myDrone.land()
+#myDrone.land()
